d7fde9faea6b9370410aab20c5e8434c0cf9983f
josielsilvaSousa <josielsilva84@gmail.com>
2023-01-13 09:26:41 -0300

    Tipos de variaveis.

--- src/trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ src/trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte _byte=127;
+        short _short=32767;
+        int _int= 2147483647;
+        long _long = 9223372036854775807L;
+        float _float = 3.4028F;//3,4028E + 38
+        double _dolble = 1.7976;  //1,7976E + 308      
+        String _string = "Josiel";
+
+        final double VALOR_DE_PI = 3.14; // Similar a constante. não pode ser alterado.
+
     }
 }
